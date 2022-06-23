dc6fda269da7405bb430beed92de1ad1d4ae28cb
Edwin Mbugua <mbugua.edwin@gmail.com>
2022-06-23 08:58:43 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "
+[server]n
+port = $PORTn
+enableCORS = falsen
+headless = truen
+n
 " > ~/.streamlit/config.toml
